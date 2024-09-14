cb1be4dc3b0079150e616e575eb1f7a4a9bae7fd
Esteban <estebancg12@hotmail.com>
2024-09-14 09:03:28 -0500

    Agregando botón para registrar vuelo 🧰

--- src/pages/gestion-de-vuelos-A/vuelos/index.tsx
+++ src/pages/gestion-de-vuelos-A/vuelos/index.tsx
@@ -2,6 +2,9 @@ import Navbar from "@/components/molecules/bars/Navbar";
 import SearchBar from "@/components/molecules/bars/SearchBar";
 import FlightCard from "@/components/molecules/cards/FlightCard";
 import FooterCopy from "@/components/molecules/FooterCopy";
+import { Button } from "@/components/ui/button";
+import { BadgePlus } from "lucide-react";
+import Link from "next/link";
 import React from "react";
 
 function FlightsHome() {
@@ -9,7 +12,16 @@ function FlightsHome() {
     <div className="flex flex-col h-full w-full">
       <Navbar />
       <SearchBar />
-      <FlightCard />
+      <section className="flex flex-col">
+        <div className="flex justify-end px-4">
+          <Link href="/gestion-de-vuelos-A/vuelos/registro">
+            <Button variant="ghost" size="lg" className="flex gap-1">
+              <BadgePlus /> Registrar vuelo
+            </Button>
+          </Link>
+        </div>
+        <FlightCard />
+      </section>
       <FooterCopy />
     </div>
   );
