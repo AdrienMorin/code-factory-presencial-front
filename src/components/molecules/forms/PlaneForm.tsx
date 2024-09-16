a745b8cb2b205d08061b5d2345775f3e57fa3358
OswaldDanielGutierrez <elnida000@gmail.com>
2024-09-16 18:03:39 -0500

    Agregando componente de busqueda de aviones

--- src/components/molecules/forms/PlaneForm.tsx
+++ src/components/molecules/forms/PlaneForm.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
-import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { PlaneSchema, formPlane } from "@/schemas/PlaneSchema";
 import { Input } from "@/components/ui/input";
 import { AlertDialog, AlertDialogAction, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogTitle, AlertDialogTrigger } from "@/components/ui/alert-dialog";
+import Image from 'next/image';
+import Link from "next/link";
+import { BadgePlus } from "lucide-react";
 
 
 function PlaneForm() {
@@ -23,7 +26,20 @@ function PlaneForm() {
       <h3 className="text-4xl font-extrabold text-center mt-12">
         Ingresar información de la Aeronave
       </h3>
-      <hr className="border-t-1 border-gray-300 w-1/2 my-10" />
+
+  
+
+      <hr className="border-t-1 border-gray-300 w-1/2 my-5" />
+
+      <div className="w-auto text-right ml-auto mr-10">
+        <Link
+          href="/gestion-de-vuelos-A/aeronaves/consulta"
+          className="flex gap-1 bg-green-600 hover:bg-green-500 transition-colors duration-200 text-slate-100 font-bold p-2 rounded-lg"
+        >
+          <BadgePlus /> Consultar Aerovanes
+        </Link>
+      </div>
+
       <Form {...form}>
         <form
           onSubmit={form.handleSubmit(onSubmit)}
@@ -105,89 +121,89 @@ function PlaneForm() {
       </h3>
 
       <Form {...form}>
-  <form
-    onSubmit={form.handleSubmit(onSubmit)}
-    className="flex flex-col md:flex-row items-center justify-center gap-4 py-6"
-  >
-    <div className="flex flex-row gap-4">
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-col md:flex-row items-center justify-center gap-4 py-6"
+        >
+          <div className="flex flex-row gap-4">
 
-      {/* DISTRIBUCION 1 */}
-      <FormField
-        control={form.control}
-        name="distribution"
-        render={({ field }) => (
-          <FormItem className="flex-1">
-            <div className="flex flex-col items-center">
-              <img src="/distribucion1.png" alt="Distribución 1" className="w-full h-auto" />
-              <button
-                type="button"
-                onClick={() => field.onChange({ target: { value: 'distribucion1' } })}
-                className={`mt-2 w-6 h-6 rounded-full border-2 ${field.value === 'distribucion1' ? 'bg-blue-500 border-black-500' : 'bg-white border-gray-300'} flex items-center justify-center`}
-              >
-                {field.value === 'distribucion1' ? (
-                  <svg className="w-4 h-4 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" />
-                  </svg>
-                ) : null}
-              </button>
-            </div>
-            <FormMessage />
-          </FormItem>
-        )}
-      />
+            {/* DISTRIBUCION 1 */}
+            <FormField
+              control={form.control}
+              name="distribution"
+              render={({ field }) => (
+                <FormItem className="flex-1">
+                  <div className="flex flex-col items-center">
+                    <Image src="/distribucion1.png" alt="Distribución 1" width={300} height={300} />
+                    <button
+                      type="button"
+                      onClick={() => field.onChange({ target: { value: 'distribucion1' } })}
+                      className={`mt-2 w-6 h-6 rounded-full border-2 ${field.value === 'distribucion1' ? 'bg-green-500 border-black-500' : 'bg-white border-gray-300'} flex items-center justify-center`}
+                    >
+                      {field.value === 'distribucion1' ? (
+                        <svg className="w-4 h-4 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" />
+                        </svg>
+                      ) : null}
+                    </button>
+                  </div>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-      {/* DISTRIBUCION 2 */}
-      <FormField
-        control={form.control}
-        name="distribution"
-        render={({ field }) => (
-          <FormItem className="flex-1">
-            <div className="flex flex-col items-center">
-              <img src="/distribucion2.png" alt="Distribución 2" className="w-full h-auto" />
-              <button
-                type="button"
-                onClick={() => field.onChange({ target: { value: 'distribucion2' } })}
-                className={`mt-2 w-6 h-6 rounded-full border-2 ${field.value === 'distribucion2' ? 'bg-blue-500 border-black-500' : 'bg-white border-gray-300'} flex items-center justify-center`}
-              >
-                {field.value === 'distribucion2' ? (
-                  <svg className="w-4 h-4 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" />
-                  </svg>
-                ) : null}
-              </button>
-            </div>
-            <FormMessage />
-          </FormItem>
-        )}
-      />
+            {/* DISTRIBUCION 2 */}
+            <FormField
+              control={form.control}
+              name="distribution"
+              render={({ field }) => (
+                <FormItem className="flex-1">
+                  <div className="flex flex-col items-center">
+                    <Image src="/distribucion2.png" alt="Distribución 2" width={300} height={300} />
+                    <button
+                      type="button"
+                      onClick={() => field.onChange({ target: { value: 'distribucion2' } })}
+                      className={`mt-2 w-6 h-6 rounded-full border-2 ${field.value === 'distribucion2' ? 'bg-green-500 border-black-500' : 'bg-white border-gray-300'} flex items-center justify-center`}
+                    >
+                      {field.value === 'distribucion2' ? (
+                        <svg className="w-4 h-4 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" />
+                        </svg>
+                      ) : null}
+                    </button>
+                  </div>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-      {/* DISTRIBUCION 3 */}
-      <FormField
-        control={form.control}
-        name="distribution"
-        render={({ field }) => (
-          <FormItem className="flex-1">
-            <div className="flex flex-col items-center">
-              <img src="/distribucion3.png" alt="Distribución 3" className="w-full h-auto" />
-              <button
-                type="button"
-                onClick={() => field.onChange({ target: { value: 'distribucion3' } })}
-                className={`mt-2 w-6 h-6 rounded-full border-2 ${field.value === 'distribucion3' ? 'bg-blue-500 border-black-500' : 'bg-white border-gray-300'} flex items-center justify-center`}
-              >
-                {field.value === 'distribucion3' ? (
-                  <svg className="w-4 h-4 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" />
-                  </svg>
-                ) : null}
-              </button>
-            </div>
-            <FormMessage />
-          </FormItem>
-        )}
-      />
-    </div>
-  </form>
-</Form>
+            {/* DISTRIBUCION 3 */}
+            <FormField
+              control={form.control}
+              name="distribution"
+              render={({ field }) => (
+                <FormItem className="flex-1">
+                  <div className="flex flex-col items-center">
+                    <Image src="/distribucion3.png" alt="Distribución 3" width={300} height={300} />
+                    <button
+                      type="button"
+                      onClick={() => field.onChange({ target: { value: 'distribucion3' } })}
+                      className={`mt-2 w-6 h-6 rounded-full border-2 ${field.value === 'distribucion3' ? 'bg-green-500 border-black-500' : 'bg-white border-gray-300'} flex items-center justify-center`}
+                    >
+                      {field.value === 'distribucion3' ? (
+                        <svg className="w-4 h-4 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" />
+                        </svg>
+                      ) : null}
+                    </button>
+                  </div>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+        </form>
+      </Form>
 
 
 
@@ -196,7 +212,7 @@ function PlaneForm() {
       {/* Botón para registrar avión */}
       <AlertDialog>
         <AlertDialogTrigger asChild>
-          <Button type="submit" className="mt-4 bg-blue-500 text-white border border-blue-500 rounded-md px-4 py-2 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-300">
+          <Button type="submit" className="mt-4  text-white border border-green-500 rounded-md px-4 py-2 hover:bg-green-500 focus:outline-none focus:ring-2 focus:ring-blue-300">
             Registrar Aerovane
           </Button>
         </AlertDialogTrigger>
