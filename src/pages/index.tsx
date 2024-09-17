18d0ffd609a95d2e2d4eb6209b671a6ed94742b3
dayanaramirez07 <dayana.ramirez@udea.edu.co>
2024-09-17 07:20:49 -0500

    Actualización

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,7 @@
-import { Button } from '@/components/ui/button';
-
 export default function Home() {
   return (
     <div className='flex justify-center items-center'>
-      <h1 className='text-6xl'>Hola Mundo probando el boton</h1>
-      <Button>Button</Button>
+      <h1 className='text-6xl'>Hello World</h1>
     </div>
   );
 }
