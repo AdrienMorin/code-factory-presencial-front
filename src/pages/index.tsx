a04a361d617b67e516a431f188efaa2e3b1a9f18
AdrienMorin <126267737+AdrienMorin@users.noreply.github.com>
2024-09-17 15:05:26 +0200

    Prueba despliegue pagos A

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,8 @@ import { Button } from '@/components/ui/button';
 export default function Home() {
   return (
     <div className='flex justify-center items-center'>
-      <h1 className='text-6xl'>Hola Mundo probando el boton</h1>
-      <Button>Button</Button>
+      <div><h1 className='text-6xl'>Hola Mundo, pagos A</h1></div>
+      <div><Button>Button</Button></div>
     </div>
   );
 }
