d8384fe58a3427fc44f5c5cd1afcfe365152a75a
DarkanCC <acar9280@gmail.com>
2024-09-17 09:50:43 -0500

    Actualizacion index y css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
-import { Button } from '@/components/ui/button';
+import { Button } from '@/src/components/ui/button';
+import { Icon } from 'lucide-react';
 import { useState } from 'react';
 
 export default function Home() {
@@ -30,27 +31,26 @@ export default function Home() {
   return (
     <>
       {/* Barra de navegación fija */}
-      <nav className="fixed top-0 left-0 w-full h-16 bg-blue-500 flex items-center justify-center shadow-md">
-        <h1 className="text-white text-lg">Mi Aplicación</h1>
+      <nav className="fixed top-0 left-0 w-full h-16 bg-sky-500 flex items-center justify-center shadow-md">
+        <span className="typcn--compass"></span>
+        <h1 className="text-white text-lg font-bold">Singapur Airlines</h1>
       </nav>
 
       {/* Contenido principal */}
-      <div className="mt-20 flex justify-center items-center">
-        <form className="w-full max-w-lg p-6 bg-white shadow-lg rounded-md" onSubmit={handleSubmit}>
-          {/* Avatar Section */}
-          <div className="mb-6">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="avatar">
-              Avatar
-            </label>
-            <input type="file" id="avatar" className="border border-gray-300 p-2 w-full" />
-          </div>
+      {/* Avatar Section */}
+      <div className="mb-6 mt-40 px-20 flex justify-end flex-col">
+        <h2 className="text-xl mb-4 font-bold">Avatar</h2>
+        <input type="file" id="avatar" className="border border-gray-300 p-2 w-full" />
+      </div>
 
-          {/* Datos Personales */}
-          <h2 className="text-xl mb-4 font-semibold">Datos Personales</h2>
+      <div className="flex flex-col px-20">
+        {/* Datos Personales */}
+        <h2 className="text-xl mb-4 font-bold">Datos Personales</h2>
+        <form className="grid grid-cols-2 gap-x-10 w-full" onSubmit={handleSubmit}>
 
           <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="nombre">
-              Nombre
+            <label className="block text-black-500 text-sm font-medium mb-2" htmlFor="nombre">
+              Nombre*
             </label>
             <input
               type="text"
@@ -58,13 +58,14 @@ export default function Home() {
               name="nombre"
               value={formData.nombre}
               onChange={handleChange}
-              className="border border-gray-300 p-2 w-full"
+              className="border border-gray-300 p-2 w-full rounded-sm"
             />
+            <p className='mt-2 text-slate-500 font-normal text-sm'>Introduzca su nombre</p>
           </div>
 
           <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="apellido">
-              Apellido
+            <label className="block text-black-500 text-sm font-medium mb-2" htmlFor="apellido">
+              Apellido*
             </label>
             <input
               type="text"
@@ -72,13 +73,14 @@ export default function Home() {
               name="apellido"
               value={formData.apellido}
               onChange={handleChange}
-              className="border border-gray-300 p-2 w-full"
+              className="border border-gray-300 p-2 w-full rounded-sm"
             />
+            <p className='mt-2 text-slate-500 font-normal text-sm'>Introduzca su apellido</p>
           </div>
 
           <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="fechaNacimiento">
-              Fecha de Nacimiento
+            <label className="block text-black-500 text-sm font-medium mb-2" htmlFor="fechaNacimiento">
+              Fecha de Nacimiento*
             </label>
             <input
               type="date"
@@ -86,13 +88,14 @@ export default function Home() {
               name="fechaNacimiento"
               value={formData.fechaNacimiento}
               onChange={handleChange}
-              className="border border-gray-300 p-2 w-full"
+              className="border border-gray-300 p-2 w-full rounded-sm"
             />
+            <p className='mt-2 text-slate-500 font-normal text-sm'>Introduzca su fecha de nacimiento</p>
           </div>
 
           <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="telefono">
-              Teléfono
+            <label className="block text-black-500 text-sm font-medium mb-2" htmlFor="telefono">
+              Teléfono*
             </label>
             <input
               type="tel"
@@ -100,13 +103,14 @@ export default function Home() {
               name="telefono"
               value={formData.telefono}
               onChange={handleChange}
-              className="border border-gray-300 p-2 w-full"
+              className="border border-gray-300 p-2 w-full rounded-sm"
             />
+            <p className='mt-2 text-slate-500 font-normal text-sm'>Introduzca su teléfono</p>
           </div>
 
           <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="ciudad">
-              Ciudad
+            <label className="block text-black-500 text-sm font-medium mb-2" htmlFor="ciudad">
+              Ciudad*
             </label>
             <input
               type="text"
@@ -114,31 +118,33 @@ export default function Home() {
               name="ciudad"
               value={formData.ciudad}
               onChange={handleChange}
-              className="border border-gray-300 p-2 w-full"
+              className="border border-gray-300 p-2 w-full rounded-sm"
             />
+            <p className='mt-2 text-slate-500 font-normal text-sm'>Introduzca su ciudad</p>
           </div>
 
           <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="genero">
-              Género
+            <label className="block text-black-500 text-sm font-medium mb-2" htmlFor="genero">
+              Género*
             </label>
             <select
               id="genero"
               name="genero"
               value={formData.genero}
               onChange={handleChange}
-              className="border border-gray-300 p-2 w-full"
+              className="border border-gray-300 p-2 w-full rounded-sm"
             >
               <option value="">Seleccionar</option>
               <option value="masculino">Masculino</option>
               <option value="femenino">Femenino</option>
               <option value="otro">Otro</option>
             </select>
+            <p className='mt-2 text-slate-500 font-normal text-sm'>Seleccione su género</p>
           </div>
 
           <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="tipoDocumento">
-              Tipo de Documento
+            <label className="block text-black-500 text-sm font-medium mb-2" htmlFor="tipoDocumento">
+              Tipo de Documento*
             </label>
             <input
               type="text"
@@ -146,13 +152,14 @@ export default function Home() {
               name="tipoDocumento"
               value={formData.tipoDocumento}
               onChange={handleChange}
-              className="border border-gray-300 p-2 w-full"
+              className="border border-gray-300 p-2 w-full rounded-sm"
             />
+            <p className='mt-2 text-slate-500 font-normal text-sm'>Seleccione su tipo de documento</p>
           </div>
 
           <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="documento">
-              Documento
+            <label className="block text-black-500 text-sm font-medium mb-2" htmlFor="documento">
+              Documento*
             </label>
             <input
               type="text"
@@ -160,13 +167,14 @@ export default function Home() {
               name="documento"
               value={formData.documento}
               onChange={handleChange}
-              className="border border-gray-300 p-2 w-full"
+              className="border border-gray-300 p-2 w-full rounded-sm"
             />
+            <p className='mt-2 text-slate-500 font-normal text-sm'>Introduzca su documento</p>
           </div>
 
           <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="direccion">
-              Dirección
+            <label className="block text-black-500 text-sm font-medium mb-2" htmlFor="direccion">
+              Dirección*
             </label>
             <input
               type="text"
@@ -174,12 +182,16 @@ export default function Home() {
               name="direccion"
               value={formData.direccion}
               onChange={handleChange}
-              className="border border-gray-300 p-2 w-full"
+              className="border border-gray-300 p-2 w-full rounded-sm"
             />
+            <p className='mt-2 text-slate-500 font-normal text-sm'>Introduzca su dirección</p>
           </div>
-
-          <Button type="submit">CREAR CUENTA</Button>
+          
         </form>
+        <Button type="submit">
+          CREAR CUENTA
+          <span className="line-md--account-add ml-2"></span>
+        </Button>
       </div>
     </>
   );
