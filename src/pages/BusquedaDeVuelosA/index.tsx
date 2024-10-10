96d6a597fea67e94bf140fb6a74133f04a587c22
JuanPablo-A <jpablo.arango1@udea.edu.co>
2024-10-10 13:34:16 -0500

    feat: :sparkles: Add Navbar, SearchForm, and PublicityCard components
    This commit adds the Navbar, SearchForm, and PublicityCard components to the BusquedaDeVuelosA page. The Navbar component displays a navigation bar at the top of the page with user profile information. The SearchForm component handles search input and calendar functionality, using the input fields and input fields with calendar functionality defined in the constants. Finally, the PublicityCard component displays a promotional card with an image and text. These UI components enhance the overall user experience of the page.

--- src/pages/BusquedaDeVuelosA/index.tsx
+++ src/pages/BusquedaDeVuelosA/index.tsx
@@ -1,9 +1,18 @@
+import Navbar from '@/components/molecules/Navbar'
+import PublicityCard from '@/components/molecules/PublicityCard'
+import SearchForm from '@/components/molecules/SearchForm'
+import { searchFields, searchFieldsCalendar } from '@/constants/FormFields'
 import React from 'react'
 
 export default function BusquedaDeVuelosA() {
   return (
-    <div>
-      
+    <div className='flex flex-col justify-center items-center gap-10'>
+      <Navbar />
+      <SearchForm 
+        inputFields={searchFields}
+        inputFieldsCalendar={searchFieldsCalendar}
+      />
+      <PublicityCard />
     </div>
   )
 }
