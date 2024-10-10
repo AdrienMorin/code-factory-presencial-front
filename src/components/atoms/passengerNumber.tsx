602217c4274739a36a8c7b5e8117d00fa263b50e
dayanaramirez07 <dayana.ramirez@udea.edu.co>
2024-10-10 00:24:34 -0500

    Estructura de carpetas

---
+++ src/components/atoms/passengerNumber.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PassengerNumber = () => {
+  return (
+    <div>Number of Passengers</div>
+  )
+}
+
+export default PassengerNumber
---
+++ src/components/molecules/flightCard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FlightCard = () => {
+  return (
+    <div>Flight Card</div>
+  )
+}
+
+export default FlightCard
