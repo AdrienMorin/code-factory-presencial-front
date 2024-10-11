852fa65dd3ac2bd4ac0253554d56d6979d34f8a7
JuanPablo-A <jpablo.arango1@udea.edu.co>
2024-10-11 01:06:10 -0500

    refactor: :sparkles: Update SearchForm component to adds state hooks for trip type and search parameters and handles form submission correctly.

--- src/components/molecules/SearchForm.tsx
+++ src/components/molecules/SearchForm.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { RadioGroup, RadioGroupItem} from '../ui/radio-group'
 import { Label } from '@radix-ui/react-label'
 import { InputSearchField, InputSearchFieldCalendar } from '@/types/InputTypes'
@@ -12,18 +12,35 @@ type SearchFormProps = {
 }
 
 export default function SearchForm( { inputFields, inputFieldsCalendar }: SearchFormProps ) {
+
+  const [tripType, setTripType] = useState('roundTrip'); 
+  const [searchParams, setSearchParams] = useState({
+    origin: '',
+    destination: '',
+    departureDate: '',
+    returnDate: '',
+    passengers: 1
+  });
+
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
-    // Aquí puedes manejar el envío del formulario
-    console.log('Formulario enviado');
+    // Aquí envías los parámetros al backend
+    console.log('Parámetros de búsqueda:', searchParams, tripType);
+  };
+
+  const handleInputChange = (name: string, value: string) => {
+    setSearchParams({
+      ...searchParams,
+      [name]: value
+    });
   };
 
   return (
-    <form onSubmit={handleSubmit} className='flex w-4/5 p-4 rounded-lg shadow-md border gap-10 justify-center items-center'>
+    <form onSubmit={handleSubmit} className='flex w-4/5 p-4 rounded-lg shadow-md border gap-10 justify-evenly items-center'>
         <RadioGroup
           defaultValue="roundTrip"
           className="flex space-x-4"
-          //onValueChange={(value) => setSearchParams({ ...searchParams, tripType: value as 'roundTrip' | 'oneWay' })}
+          onValueChange={(value) => setTripType(value)}
         >
           <div className="flex items-center space-x-2">
             <RadioGroupItem value="roundTrip" id="roundTrip" />
@@ -36,11 +53,13 @@ export default function SearchForm( { inputFields, inputFieldsCalendar }: Search
         </RadioGroup>
         <SearchInput 
           inputSearchField={inputFields} 
+          handleInputChange={handleInputChange}
         />
         <SearchInputCalendar 
           inputSearchField={inputFieldsCalendar}
+          handleInputChange={handleInputChange}
         />
-        <Button type="submit" className="bg-primary text-white rounded-lg p-2">Buscar</Button>
+        <Button type="submit" className="bg-primary w-20 text-white rounded-xl p-2">Buscar</Button>
     </form>
   )
 }
