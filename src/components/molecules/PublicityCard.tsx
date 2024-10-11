affc8cdfaf2b8f8bf22093daa2cf2b28d5f4a93c
JuanPablo-A <jpablo.arango1@udea.edu.co>
2024-10-11 12:29:21 -0500

    refactor: :lipstick: Adjust PublicityCard component layout and styles

--- src/components/molecules/PublicityCard.tsx
+++ src/components/molecules/PublicityCard.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 export default function PublicityCard() {
   return (
-    <div className='flex w-4/5 rounded-3xl shadow-md border gap-10 items-center bg-card'>
-      <Image src='/image.png' alt='publicity' width={600} height={400} /> 
-      <div className='flex flex-col w-3/6'>
+    <div className='flex flex-col lg:w-4/5 lg:flex-row rounded-3xl shadow-md border gap-10 lg:items-center bg-card'>
+      <Image src='/image.png' alt='publicity' width={600} height={400} className='w-full h-full'/> 
+      <div className='w-full p-5'>
         <h1 className='text-4xl font-bold'>Cada viaje es una historia por contar</h1>
         <p>Reserva y vuela hasta noviembre 2024</p>
         <p className='mt-5'>Por trayecto desde:</p>
