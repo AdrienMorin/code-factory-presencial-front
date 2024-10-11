e7d9f076461c4142beb1b73fc03b17179e775de5
FerleySilva04 <ferley.silvaj@udea.edu.co>
2024-10-11 17:46:53 -0500

    Nuevos cambios

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,6 @@
-// pages/_app.tsx
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import Link from "next/link";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <nav>
-        <ul>
-          <li>
-            <Link href="/equipajes/home">Home</Link>
-          </li>
-          <li>
-            <Link href="/equipajes/agregarequipaje">Agregar Equipaje</Link>
-          </li>
-        </ul>
-      </nav>
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,38 @@
-import Head from 'next/head';
-import { Header } from '../components/molecules/Header'; // Asegúrate de que la ruta sea correcta
+import { useRouter } from "next/router"; // Importa useRouter
+import { Header } from "@/components/molecules/Header"; 
+import { Footer } from "@/components/atoms/Footer"; 
+import { Button } from "@/components/atoms/button"; 
+import { Label } from "@/components/atoms/Label";
 
 export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>Prueba del Header</title>
-        <meta name="description" content="Probando el Header con ShadcnUI" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+  const router = useRouter(); // Inicializa el router
 
-      {/* Header */}
+  return (
+    <div className="min-h-screen flex flex-col justify-between">
       <Header />
 
-      {/* Puedes agregar más contenido debajo del header para probar el comportamiento */}
-      <main className="flex justify-center items-center h-screen bg-gray-100">
-        <h1 className="text-4xl">Contenido debajo del Header</h1>
+      <main className="flex-grow flex flex-col items-center justify-start text-center p-4 space-y-4" style={{ marginTop: '3.5rem' }}>
+        <Label className="text-3xl text-black mb-1 font-bold no-shadow">¿Deseas añadir equipaje?</Label> {/* Título en negrita */}
+        <Label className="text-gray-600 text-xs mb-4">Añade tu equipaje para el viaje reservado</Label> {/* Texto en tamaño pequeño */}
+        
+        <div className="flex space-x-2">
+          <Button 
+            className="bg-white text-black border border-gray-300 text-sm" // Botón de color blanco
+            onClick={() => {/* Redirigir a la página de omitir */}}
+          >
+            Omitir
+          </Button>
+          
+          <Button 
+            className="bg-customSky text-white text-sm" // Botón de color customSky
+            onClick={() => router.push("/equipajes/agregarequipaje")} // Redirigir a la ruta deseada
+          >
+            Añadir equipaje
+          </Button>
+        </div>
       </main>
+      
+      <Footer /> {/* Footer agregado al final */}
     </div>
   );
 }
