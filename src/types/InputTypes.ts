537c9d611831d3ba54ca2bdc3dc3bf5512f3dd12
JuanPablo-A <jpablo.arango1@udea.edu.co>
2024-10-15 08:16:26 -0500

    feat: :sparkles: Add Flight type definition.

--- src/types/InputTypes.ts
+++ src/types/InputTypes.ts
@@ -10,6 +10,13 @@ export type InputSearchFieldPassenger = Omit<InputSearchField, 'value'> & {
   value: string;
 }
 
+export type InputSearchFieldTypeFlight = {
+  label: string;
+  value: string;
+  placeholder: string;
+  type: string;
+}
+
 export type InputSearchFieldCalendar = {
   label: string;
   value: string;
