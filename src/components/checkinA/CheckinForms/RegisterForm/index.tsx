f8f8eb58535ac32ca4213d83d63f2093fe9a7642
danielcanoh22 <daniel.canoh@udea.edu.co>
2024-10-21 18:31:26 -0500

    Add test user :rocket:

--- src/components/checkinA/CheckinForms/RegisterForm/index.tsx
+++ src/components/checkinA/CheckinForms/RegisterForm/index.tsx
@@ -9,8 +9,17 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+
+const TEST_USERS = [
+  {
+    fullName: "Alejandra Díaz",
+    code: "ADH",
+    ticket: "14101",
+  },
+];
 
 const formSchema = z.object({
   lastName: z.string(),
@@ -25,6 +34,22 @@ export const RegisterForm = () => {
       ticketNumber: "",
     },
   });
+
+  const [isValidated, setIsValidated] = useState(false);
+  const [hasSearched, setHasSearched] = useState(false);
+
+  const handleSearchPassenger = (passengerCode: string) => {
+    if (!form.getValues().lastName || !form.getValues().ticketNumber) return;
+
+    setHasSearched(true);
+
+    const passengerExists = TEST_USERS.some(
+      (user) => user.code === passengerCode || user.ticket === passengerCode
+    );
+
+    if (passengerExists) setIsValidated(true);
+    else setIsValidated(false);
+  };
 
   return (
     <div className="flex flex-col gap-4 w-full mt-10">
@@ -62,7 +87,24 @@ export const RegisterForm = () => {
             )}
           />
 
-          <Button type="submit" className="w-full">
+          {hasSearched &&
+            (isValidated ? (
+              <p className="text-green-700 font-semibold">
+                El pasajero con el código {form.getValues().ticketNumber} ya
+                realizó el CheckIn.
+              </p>
+            ) : (
+              <p className="text-red-700 font-semibold">
+                El pasajero con el código {form.getValues().ticketNumber} no ha
+                realizado el CheckIn.
+              </p>
+            ))}
+
+          <Button
+            type="submit"
+            className="w-full"
+            onClick={() => handleSearchPassenger(form.getValues().ticketNumber)}
+          >
             Buscar
           </Button>
         </form>
