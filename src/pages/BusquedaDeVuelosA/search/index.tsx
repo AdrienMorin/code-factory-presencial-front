fbec65911dc325c202a3afe490e243d553f065bc
JuanPablo-A <jpablo.arango1@udea.edu.co>
2024-10-25 12:13:39 -0500

    refactor: :fire: Remove NavbarSearch component and adjust styling in search/index.tsx

--- src/pages/BusquedaDeVuelosA/search/index.tsx
+++ src/pages/BusquedaDeVuelosA/search/index.tsx
@@ -1,4 +1,3 @@
-import NavbarSearch from '@/components/molecules/NavbarSearch';
 import SearchForm from '@/components/organisms/SearchForm';
 import SearchResults from '@/components/organisms/searchResults';
 import { searchFields, searchFieldsCalendar, searchFieldsPassenger } from '@/constants/FormFields';
@@ -8,8 +7,7 @@ import React from 'react'
 export default function search() {
 
   return (
-    <div className='flex flex-col gap-5 justify-center items-center'>
-      <NavbarSearch/>
+    <div className='flex flex-col gap-5 justify-center items-center mt-10'>
       <SearchForm
         inputFields={searchFields}
         inputFieldsCalendar={searchFieldsCalendar}
