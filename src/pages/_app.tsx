a694c5e20254827176639a6d60c9229770d4f9b3
JuanPablo-A <jpablo.arango1@udea.edu.co>
2024-10-25 12:09:35 -0500

    refactor: :recycle: Update _app.tsx to add toast functionality

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import Navbar from "@/components/molecules/Navbar";
+import { ToastProvider } from "@/components/ui/toast";
+import { Toaster } from "@/components/ui/toaster";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ToastProvider>
       <Navbar />
       <Component {...pageProps} />
-    </>
+      <Toaster />
+    </ToastProvider>
   );
 }
